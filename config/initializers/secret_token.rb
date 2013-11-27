88e40f185e56accfec98a38a6917d2cca21a9b93
apurvagu <apurvagupta0@gmail.com>
2013-11-27 19:44:32 +0530

    project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PincodeApi::Application.config.secret_key_base = '98c8abc32cd7f9e7451de7b914644e1ed3a5fba3f72929f89dcfc742c74b3873ddb2009021d5db3ce0e1ce5a3585d7211bc361b182faffbf6a1fdfc8c34a7645'
